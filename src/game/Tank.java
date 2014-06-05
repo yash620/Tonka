1a8b34a7d5b203ee397468d66d282734136e2966
Glynn Williams <glynn96@gmail.com>
2014-06-05 09:42:05 -0700

    jnlkhb

--- src/game/Tank.java
+++ src/game/Tank.java
@@ -22,6 +22,7 @@ import weapon.Weapon;
 
 public class Tank implements Drawable, Collidable, Updatable, Sendable {
 	private double hp;
+	private double maxHP;
 	private double speed;
 	private double turnSpeed;
 	private double theta;
@@ -44,7 +45,8 @@ public class Tank implements Drawable, Collidable, Updatable, Sendable {
 		this.xcenter = x;
 		this.ycenter = y;
 		this.myWeapons = weapons;
-		this.hp = 100;
+		this.maxHP = 100;
+		hp = maxHP;
 		this.speed = 3;
 		this.turnSpeed = 5;
 		setColor(Color.green);
@@ -61,7 +63,7 @@ public class Tank implements Drawable, Collidable, Updatable, Sendable {
 		g2.setColor(Color.black);
 		g2.draw(tankShape);
 		g2.setColor(Color.red);
-		g2.fillRect((int)xcenter - 25, (int)ycenter - 30, (int)(((double)this.hp)/100 * 50), 5);
+		g2.fillRect((int)xcenter - 25, (int)ycenter - 30, (int)(this.hp/this.maxHP * 50), 5);
 		g2.setColor(Color.black);
 		g2.drawRect((int)xcenter - 25, (int)ycenter - 30, 50, 5);
 		//g2.drawString("HP" + this.hp, (int)xcenter, (int)ycenter);
